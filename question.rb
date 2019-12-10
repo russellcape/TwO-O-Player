6acd15165b2bf751c09cf2d755c21a4e907a0d3b
Russell Cape <russellcape98@gmail.com>
2019-12-10 16:00:52 +0000

    Added the logic for asking a question and checking if it's correct

--- question.rb
+++ question.rb
@@ -2,7 +2,21 @@ class Questions
   attr_reader :question, :answer
 
   def initialize (player)
-    
+    @player = player
+  end
+
+  def ask
+    ask_question
+    puts question
+  end
+
+  def correct? (answer)
+    if answer == @answer
+      puts 'Great Job!!! You are correct!'
+      true
+    else
+      puts 'Sorry... Your answer was incorrect.'
+      false
   end
 
 end
