1299cb629ce4d3206b49128f74d3286082911095
Russell Cape <russellcape98@gmail.com>
2019-12-10 15:42:32 +0000

    first commit

---
+++ game.rb
@@ -0,0 +1,9 @@
+require_relative 'player'
+
+class Game
+
+  def initialize(name)
+    
+  end
+
+end
---
+++ question.rb
@@ -0,0 +1,8 @@
+class Questions
+  attr_reader :question, :answer
+
+  def initialize (player)
+    
+  end
+
+end
